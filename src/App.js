2013a21b0a0182dc32aecbb3825092003997801b
wushiwen123456 <1131634405@qq.com>
2020-09-07 05:21:34 +0800

    start

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch,Route,HashRouter } from 'react-router-dom'
+import './App.css';
+// 引用组件
+import Home from './views/Home'
+import About from './views/About'
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+  render(){
+    return (
+      <HashRouter>
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={About} path="/about"/>
+        </Switch>
+      </HashRouter>
+    )
+  }
+}
+export default App;
